9d707ad9afe4e10913874eb5b12c43055fb19cdb
Darren,Zhang [DZ] <sqz777isme@gmail.com>
2018-07-03 17:38:10 +0800

    init complete

---
+++ MemoryTogether/Controller/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MemoryTogether.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace MemoryTogether.Controller
+{
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        [Route("home/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("home/greet/{username}")]
+        public IActionResult Greet(string username)
+        {
+            var name = new User { Name = username };
+            return Ok(name);
+        }
+    }
+}
---
+++ MemoryTogether/Model/User.cs
@@ -0,0 +1,7 @@
+﻿namespace MemoryTogether.Model
+{
+    internal class User
+    {
+        public string Name { get; set; }
+    }
+}
